a7bdbf608d69e30fdda67d359d7995b38e5113c0
Subhradeep Basu <111586851+SubhradeepBasu18@users.noreply.github.com>
2023-12-18 01:24:18 +0530

    Update TopView.c

--- Trees/TopView.c
+++ Trees/TopView.c
@@ -1,3 +1,16 @@
+/*
+Approach: 
+
+First we have to make a queue of pair which have nodes and their respective +ve and -ve indexes.
+Then we need a map data structure to store the lines and the nodes. This map will store the data in the form of sorted orders of keys(Lines).
+Here we will follow the level order traversal.
+Traverse through the nodes starting with root,0 and store them to the queue.
+Until the queue is not empty, store the node  and line no. in 2 separate variable .
+Then check if that line is present in the map or not
+If not present then store the line and the node->val to the map
+Otherwise store the node->left and node->right along with there line nos. to the queue.
+Then print the node->val from the map
+*/
 #include <stdio.h>
 #include <stdlib.h>
 
